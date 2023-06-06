96b82ecb35d19a204687c34cbacf5ddac080baac
HiramHernandez <hhg.mzt@gmail.com>
2023-06-06 16:51:48 -0600

    chore: practica de gestión de promesas asincronas con Promise

--- src/app/components/rounting/librolista/librolista.component.ts
+++ src/app/components/rounting/librolista/librolista.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/models/book.model';
 import { BOOKS } from 'src/app/mocks/books.mock';
-
+import { LibroService } from 'src/app/services/libro.service';
+import { LoggerService } from 'src/app/services/logger.service';
 @Component({
   selector: 'app-librolista',
   templateUrl: './librolista.component.html',
@@ -9,10 +10,36 @@ import { BOOKS } from 'src/app/mocks/books.mock';
 })
 export class LibrolistaComponent implements OnInit {
   books: Book[] = [];
-  constructor() { }
+  constructor(private libroService: LibroService, private loggerService: LoggerService) { }
 
   ngOnInit(): void {
-    this.books = BOOKS;
+
+    this.libroService.getLibros()
+    .then(
+      libros => {
+        this.loggerService.log("Ejecucion del 1º then");
+        return libros;
+      }
+    )
+    .then(
+      libros => {
+        this.loggerService.log("Ejecucion del 2º then");
+        return new Promise<Book[]>((resolve, reject) => { // (*)
+            this.loggerService.log("Inicio ejecutor (Promise del 2º then)");
+            setTimeout(() => {
+              this.loggerService.log("Fin ejecutor (Promise del 2º then)");
+              resolve(libros);
+            }, 5000);
+          });
+      }
+    )
+    .then(
+      libros => {
+        this.loggerService.log("Ejecucion del 3º then");
+        this.books = libros;
+      }
+    );
+
   }
 
 }
